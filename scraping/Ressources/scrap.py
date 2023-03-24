8d86668814aa56fffc5be820e866b288ab20e4d6
teslazap <49252717+OussamaElouarti@users.noreply.github.com>
2023-03-24 01:53:16 +0000

    Update scrap.py

--- scraping/Ressources/scrap.py
+++ scraping/Ressources/scrap.py
@@ -1,27 +1,30 @@
 import requests
 
 # get all the directories
-def get_paths(ip):
-    tab=[]
-    fp = requests.get(ip).text
+def get_paths(ip, path=""):
+    fp = requests.get(ip+path).text
     fp = fp.split("\n")
+    paths = []
     for i in fp:
         if len(i)>2 and i[0] == '<' and i[1] == 'a':
-            tab+=[i[i.find('\"')+1:i.find('/')+1]]
-    tab.pop()
-    return tab
+            paths.append(i[i.find('\"')+1:i.find('/')+1])
+    if paths:
+        paths.pop()
+    return paths
 
+def get_flag(ip, path=""):
+    response = requests.get(ip+path+'README').text
+    for c in response:
+        if c.isdigit():
+            print("\033[92mFound:\033[0m", end='')
+            print(response)
+            exit()
+    sub_paths = get_paths(ip, path)
+    for sub_path in sub_paths:
+        print("\033[93mSearching path : \033[0m", end='')
+        print(path+sub_path)
+        get_flag(ip, path+sub_path)
 
 ip = "http://10.12.176.69/.hidden/"
-tab1=get_paths(ip)
-tab2=get_paths(ip+tab1[0])
-tab3=get_paths(ip+tab1[0]+tab2[0])
-print("starting...")
-for i in tab1:
-    for j in tab2:
-        for k in tab3:
-            mystr = requests.get(ip+i+j+k+'README').text
-            for b in mystr:
-                if b.isdigit():
-                    print(mystr)
-                    exit()
+print("\033[91mStarting...\033[0m")
+get_flag(ip)
